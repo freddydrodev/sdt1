beb7ca69929a7b9cdbef6d1957d06a047ad3766a
Fredius Tout Court <frediustcDev@gmail.com>
2018-04-07 15:25:34 +0000

    rss feed

--- php/includes/rss.php
+++ php/includes/rss.php
@@ -1,11 +1,44 @@
-<h6>RSS Feed</h6>
-<div class="feeds">
-    <?php 
-    $i = 0;
-    while($i < 5) { ?>
-    <p><a href="#" >link for a new</a></p>
-    <?php
-        $i++;
-    }
-    ?>
-</div>
+<!-- start sw-rss-feed code --> 
+<script type="text/javascript"> 
+<!-- 
+rssfeed_url = new Array(); 
+rssfeed_url[0]="https://iheartdogs.com/feed/";  
+rssfeed_frame_width="220"; 
+rssfeed_frame_height="250"; 
+rssfeed_scroll="on"; 
+rssfeed_scroll_step="5"; 
+rssfeed_scroll_bar="off"; 
+rssfeed_target="_blank"; 
+rssfeed_font_size="15"; 
+rssfeed_font_face="inherit"; 
+rssfeed_border="off"; 
+rssfeed_css_url="https://feed.surfing-waves.com/css/style4.css"; 
+rssfeed_title="on"; 
+rssfeed_title_name=""; 
+rssfeed_title_bgcolor="#3366ff"; 
+rssfeed_title_color="#fff"; 
+rssfeed_title_bgimage=""; 
+rssfeed_footer="off"; 
+rssfeed_footer_name="rss feed"; 
+rssfeed_footer_bgcolor="#fff"; 
+rssfeed_footer_color="#333"; 
+rssfeed_footer_bgimage=""; 
+rssfeed_item_title_length="50"; 
+rssfeed_item_title_color="#666"; 
+rssfeed_item_bgcolor="#fff"; 
+rssfeed_item_bgimage=""; 
+rssfeed_item_border_bottom="on"; 
+rssfeed_item_source_icon="off"; 
+rssfeed_item_date="off"; 
+rssfeed_item_description="on"; 
+rssfeed_item_description_length="120"; 
+rssfeed_item_description_color="#666"; 
+rssfeed_item_description_link_color="#333"; 
+rssfeed_item_description_tag="off"; 
+rssfeed_no_items="0"; 
+rssfeed_cache = "5f634c5f148ad8cc3b825e208aac5395"; 
+//--> 
+</script> 
+<script type="text/javascript" src="//feed.surfing-waves.com/js/rss-feed.js"></script> 
+<!-- The link below helps keep this service FREE, and helps other people find the SW widget. Please be cool and keep it! Thanks. --> 
+<!-- end sw-rss-feed code -->
